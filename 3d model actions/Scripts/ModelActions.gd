fdbb267a30152e4c6c170a2cdc96c64676fed69a
molotgor <61899414+molotgor@users.noreply.github.com>
2020-10-22 21:02:58 +0400

    Add files via upload

--- 3d model actions/Scripts/ModelActions.gd
+++ 3d model actions/Scripts/ModelActions.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-export(float, 1, 10, 0.1) var animSpeed
+export(float, 1, 5, 0.1) var animSpeed
 var anim = 0
 var pause = false
 var animating = false
@@ -22,12 +22,12 @@ func connectAll():
 	var allNodes = get_parent()
 	#Требование след анимации вызывает nextAnim
 	allNodes.connect("nextAnim", self, "nextAnim")
-	allNodes.get_node("UI/btn_nextAnim").connect("pressed", self, "nextAnim")
+	allNodes.get_node("UI_anim/buttons/btn_nextAnim").connect("pressed", self, "nextAnim")
 	#Требование пред анимации вызывает prevAnim
 	allNodes.connect("prevAnim", self, "prevAnim")
-	allNodes.get_node("UI/btn_prevAnim").connect("pressed", self, "prevAnim")
+	allNodes.get_node("UI_anim/buttons/btn_prevAnim").connect("pressed", self, "prevAnim")
 	#Получение слайдера для скорости и связка его изменения с изменением скорости анимации
-	var slider_speedAnim = allNodes.get_node("UI/slider_speedAnim")
+	var slider_speedAnim = allNodes.get_node("UI_anim/slider_speedAnim")
 	slider_speedAnim.connect("value_changed", self, "speedChange")
 	#Установка стартового значения в стартовую позицию слайдера
 	slider_speedAnim.value = animSpeed
--- 3d model actions/Scripts/Spatial.gd
+++ 3d model actions/Scripts/Spatial.gd
@@ -2,7 +2,10 @@ extends Spatial
 
 
 onready var camera = get_node("CameraSpatial")
-var strength =  0.15
+onready var cameraMove = camera.get_child(0)
+onready var slider_cameraMove = get_node("Ui_camera/slider_cameraMove")
+var rotateStrength = 0.15
+var moveStrength = 0.5
 var swipe_strength = 0.01
 var swipe_start 
 var swipe_end
@@ -12,18 +15,21 @@ signal prevAnim()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	#get_node("Zoom/btn_closerZoom").connect("pressed", self, "closerZoom")
+	#get_node("Zoom/btn_fartherZoom").connect("pressed", self, "fartherZoom")
+	get_node("Ui_camera/btn_resetCamera").connect("pressed", self, "resetCamera")
+	slider_cameraMove.connect("value_changed", self, "cameraZoom")
+	slider_cameraMove.value = cameraMove.translation.z
+	
 
 func _process(_delta):
 	var xDir = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	var yDir = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	camera.rotation += Vector3(yDir * strength, xDir * strength, 0)
+	camera.rotation += Vector3(yDir * rotateStrength, xDir * rotateStrength, 0)
 	if Input.is_action_just_pressed("ui_nextAnim"): 
-		#camera.rotation = Vector3(0, 0, 0)
 		emit_signal("nextAnim")
 		
 	if Input.is_action_just_pressed("ui_prevAnim"): 
-		#camera.rotation = Vector3(0, 0, 0)
 		emit_signal("prevAnim")
 		
 	if Input.is_action_just_pressed("ui_click"):
@@ -37,4 +43,10 @@ func _process(_delta):
 		camera.rotation += Vector3((swipe_start.y - swipe_end.y) * swipe_strength, (swipe_start.x - swipe_end.x) * swipe_strength, 0)
 		swipe_start = get_viewport().get_mouse_position()
 		
+func cameraZoom(value):
+	cameraMove.translation = Vector3(0, 0, value)
 	
+func resetCamera():
+	camera.rotation = Vector3(-.3, 0, 0)
+	cameraMove.translation = Vector3(0, 0, 4)
+	slider_cameraMove.value = 4
